1353865915a11259c8a82f222e61a94a65f585d9
JW9506 <cf.dota@gmail.com>
2023-12-18 14:00:26 -0600

    init Order service

---
+++ order-service/src/main/java/com/plantshop/OrderApp.java
@@ -0,0 +1,12 @@
+package com.plantshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApp.class, args);
+	}
+}
---
+++ order-service/src/main/java/com/plantshop/controller/OrderController.java
@@ -0,0 +1,13 @@
+// package com.plantshop.controller;
+
+// import com.plantshop.response.BaseResponse;
+
+// @RestController
+// @RequestMapping("/api/order")
+// public class OrderController {
+
+//     @GetMapping("/{id}")
+//     public BaseResponse getOrder(@PathVariable("id") String id) {
+//         return ;
+//     }
+// }
