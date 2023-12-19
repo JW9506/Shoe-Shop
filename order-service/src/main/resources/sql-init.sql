7be7de6464427cf1a478395b950851dc469ce021
JW9506 <cf.dota@gmail.com>
2023-12-18 19:58:43 -0600

    sql init script fix

--- order-service/src/main/resources/sql-init.sql
+++ order-service/src/main/resources/sql-init.sql
@@ -1,16 +1,16 @@
 
 create table t_customer (
-  id int primary key,
+  id int AUTO_INCREMENT primary key,
   name varchar(255),
   city varchar(255)
 );
 create table t_order (
-  id int primary key,
+  id int AUTO_INCREMENT primary key,
   customer_id int,
   details varchar(255),
   foreign key (customer_id) references t_customer(id)
 );
 
-insert into t_customer values (1, 'jacky', 'egypt');
-insert into t_customer values (2, 'kacy', 'dubai');
-insert into t_order values (1, 1, 'this is some order details');
+insert into t_customer (name, city) values ('jacky', 'egypt');
+insert into t_customer (name, city) values ('kacy', 'dubai');
+insert into t_order (customer_id, details) values (1, 'this is some order details');
