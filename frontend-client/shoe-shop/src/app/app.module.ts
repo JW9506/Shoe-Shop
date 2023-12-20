75bce60caf462600b8108f5e14fc23b297ad9abc
JW9506 <cf.dota@gmail.com>
2023-12-20 15:03:31 -0600

    Render MaterialUI components, architect structure inside src/app

--- frontend-client/shoe-shop/src/app/app.module.ts
+++ frontend-client/shoe-shop/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -12,6 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
     BrowserAnimationsModule
   ],
   providers: [],
