712dd7f07166536197f9546ffbf11b80412ca144
JW9506 <cf.dota@gmail.com>
2023-12-20 15:55:43 -0600

    Lift cross origin request

---
+++ product-service/src/main/java/com/shoeshop/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.shoeshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // TODO: Should read from application.yml
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
