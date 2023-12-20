163ee226ca987c4813c2ce54783b95deac4d480a
JW9506 <cf.dota@gmail.com>
2023-12-20 12:29:33 -0600

    Add Cart and CartItem repository

---
+++ order-service/src/main/java/com/shoeshop/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.shoeshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.shoeshop.entity.Cart;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
