08ef4250ddd11e75147b8981e1de39fd7b1f943c
JW9506 <cf.dota@gmail.com>
2023-12-22 13:39:39 -0600

    Cart page can render cart store data

--- frontend-client/shoe-shop/src/app/pages/cart/cart.component.ts
+++ frontend-client/shoe-shop/src/app/pages/cart/cart.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { CartStore } from 'src/app/core/store/cart.store';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  cartItems$ = this.cartStore.select(s => s.cartItems);
+  count$ = this.cartStore.select(s => s.count);
+
+  constructor(private cartStore: CartStore) {}
+
+  ngOnInit(): void {
+  }
 }
