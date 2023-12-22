641d60eb207b278d3a69f86d9635ea07bb9e7676
JW9506 <cf.dota@gmail.com>
2023-12-22 12:01:30 -0600

    init cart module

--- frontend-client/shoe-shop/src/app/app-routing.module.ts
+++ frontend-client/shoe-shop/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cart',
+    loadChildren: () => import('./feature/cart/cart.module').then(m => m.CartModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-client/shoe-shop/src/app/app.module.ts
+++ frontend-client/shoe-shop/src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Interceptor } from './core/network/Interceptor';
+import { CategoryStore } from './core/store/category.store';
 import { CategoryListModule } from './feature/category-list/category-list.module';
 import { ProductListModule } from './feature/product-list/product-list.module';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreModule } from '@ngrx/store';
-import { CategoryStore } from './core/store/category.store';
 import { LoadingComponent } from './shared/components/loading/loading.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Interceptor } from './core/network/Interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingComponent
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
