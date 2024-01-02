8bf835faf9646eb7f3e4a76333b6c88c7359b769
JW9506 <cf.dota@gmail.com>
2024-01-02 10:30:36 -0600

    Fix inventory-module building problem

--- inventory-service/src/main/java/com/shoeshop/InventoryApp.java
+++ inventory-service/src/main/java/com/shoeshop/InventoryApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import com.shoeshop.config.AuthPartyProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing
--- inventory-service/src/main/java/com/shoeshop/controller/InventoryController.java
+++ inventory-service/src/main/java/com/shoeshop/controller/InventoryController.java
@@ -1,24 +1,7 @@
 package com.shoeshop.controller;
 
-import static com.shoeshop.response.SuccessInfo.SUCCESS;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.shoeshop.config.AuthPartyProperties;
-import com.shoeshop.dto.AuthResponseDto;
-import com.shoeshop.response.DataResponse;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
